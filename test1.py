55d1783139f4ccc6e41c79812920785b1eaea65e
Mark Chaisson <mchaisso@uw.edu>
2014-09-26 11:59:20 -0700

    deleting unused files part 2

--- test1.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-import subprocess
-
-
-command = "/net/eichler/vol5/home/mchaisso/projects/PacBioSequencing/scripts/test2.py"
-
-subprocess.call(command.split())
-
--- test2.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-
-
-import subprocess
-
-command = "ls -l "
-subprocess.call(command.split())
